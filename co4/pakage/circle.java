3d7e997ace6ddfe61cac4c149a63461b783a6c03
sufail ahmd <97265575+sufailn@users.noreply.github.com>
2023-09-06 19:48:50 +0530

    circle.java

--- co4/pakage/circle.java
+++ co4/pakage/circle.java
@@ -1,52 +1,24 @@
+package graphics;
 import java.util.Scanner;
-class Student{
-    String Name;
-    int a_score;
-    int roll_no;
-    Student(String Name,int a_score,int roll_no){
-        this.Name=Name;
-        this.a_score=a_score;
-        this.roll_no=roll_no;
-    }
+interface Area3
+{
+void get3();
+void show3();
 }
-class Sports extends Student{
-    String sport_item;
-    int s_score;
-    Sports(String sport_item,int s_score,String Name,int a_score,int roll_no){
-        super(Name, a_score, roll_no);
-        this.sport_item=sport_item;
-        this.s_score=s_score;
-    }
+public class Circle implements Area3
+{
+Scanner sc=new Scanner(System.in);
+int r;
+double a3;
+double pi=3.14;
+public void get3()
+{
+System.out.println("Enter the radius:");
+r=sc.nextInt();
 }
-class Result extends Sports{
-    int total;
-    Result(String sport_item,int s_score,String Name,int a_score,int roll_no){
-        super(sport_item, s_score, Name, a_score, roll_no);
-        total=s_score+a_score;
-    }
-    void Display(){
-        System.out.println("Roll Number:"+roll_no);
-        System.out.println("Nmae:"+Name);
-        System.out.println("Academic Score:"+a_score);
-        System.out.println("Sports item:"+sport_item);
-        System.out.println("Sports Score:"+s_score);
-        System.out.println("Total Score:"+total);
-    }
+public void show3()
+{
+a3=pi*r*r;
+System.out.println("The area of circle is:" +a3);
 }
-class Main5{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your Roll Number:");
-        int r=sc.nextInt();
-        System.out.println("Enter your Name:");
-        String name=sc.next();
-        System.out.println("Enter your Academic Score:");
-        int a=sc.nextInt();
-        System.out.println("Enter the name of the sport item you participated:");
-        String sport=sc.next();
-        System.out.println("Enter your Sports score:");
-        int s=sc.nextInt();
-        Result obj=new Result(sport, s, name, a, r);
-        obj.Display();
-    }
 }
